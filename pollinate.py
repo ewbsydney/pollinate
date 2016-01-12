9f174d7a69869c137878abe37b9aceab08a5e1cc
Matthew Imhoff <m.imhoff@acfr.usyd.edu.au>
2016-01-12 14:23:23 +1100

    starting a framework for the pollinate project...

--- pollinate.py
+++ pollinate.py
@@ -1,4 +1,3 @@
-import numpy
 import cv2
 
 # load a satellite image
@@ -10,3 +9,12 @@ import cv2
 # analyse a tile
 # input: a tile
 # output: a classification
+
+img = cv2.imread('/home/matthewi/Pictures/reignite.png')
+grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+edges = cv2.Canny(grey, 100, 200)
+
+cv2.imwrite("edges.bmp", edges)
+
+# list of points -> kml
