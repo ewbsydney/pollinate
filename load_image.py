54ba798b2c46cea69733923e72402cc3a311fbf7
Matthew Imhoff <m.imhoff@acfr.usyd.edu.au>
2016-01-12 11:56:49 +1100

    starting a framework for the pollinate project...

---
+++ load_image.py
@@ -0,0 +1,12 @@
+import numpy
+import cv2
+
+# load a satellite image
+# inputs: image file, length, width, location info (scale image distance to real distance, location of image)
+# outputs: image object
+
+# break up an image into tiles
+
+# analyse a tile
+# input: a tile
+# output: a classification
